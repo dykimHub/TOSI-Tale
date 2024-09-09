06094ab18d38c363737178801a6eade3e2854e64
김다윤 <dy.jo.kim@gmail.com>
2024-09-10 00:18:02 +0900

    Feat: S3 객체 파일, 폴더, 메타 데이터 조회 메서드

--- backend/tale/src/main/java/com/tosi/tale/s3/S3Service.java
+++ backend/tale/src/main/java/com/tosi/tale/s3/S3Service.java
@@ -1,66 +1,18 @@
 package com.tosi.tale.s3;
 
-import com.amazonaws.services.s3.AmazonS3;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class S3Service {
-    private final AmazonS3 amazonS3;
-    private final S3Client s3Client;
+public interface S3Service {
 
-    private String bucketName = "talebucket";
+    S3Object findS3Object(String s3Key);
 
-    public String getPath(String fileName) {
-        return amazonS3.getUrl(bucketName, fileName).toString();
-    }
-//
-//    public String uploadFile(MultipartFile multipartFile, String fileName) throws IOException {
-//        File file = convertMultiPartFileToFile(multipartFile);
-//        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file));
-//        file.delete();
-//        return amazonS3.getUrl(bucketName, fileName).toString();
-//    }
-//
-//    private File convertMultiPartFileToFile(MultipartFile multipartFile) throws IOException {
-//        File file = new File(multipartFile.getOriginalFilename());
-//        try (FileOutputStream fos = new FileOutputStream(file)) {
-//            fos.write(multipartFile.getBytes());
-//        }
-//        return file;
-//    }
+    String findS3URL(String s3key);
 
-    public String uploadImageFromUrl(String imageUrl) {
-        String fileName = convertUrlToFileName(imageUrl);
-        try {
-            URL url = new URL(imageUrl);
+    String[] findContents(String s3Key);
 
-            System.setProperty("java.net.useSystemProxies", "false");
-            InputStream inputStream = url.openStream();
-            byte[] imageBytes = inputStream.readAllBytes();
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(fileName)
-                    .contentType("image/png")
-                    .build();
-            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(imageBytes));
+    String[] findCharacters(String s3Key);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("IOException Details: " + e.getMessage());
-        }
-        return fileName;
-    }
-    private String convertUrlToFileName(String imageUrl) {
-        String fileName = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
-        fileName = fileName.replaceAll("[^a-zA-Z0-9.-]", "_"); // 영문 대소문자, 숫자, 점(.)을 제외한 문자를 밑줄(_)로 대체
-        return fileName;}
+    List<String> findImages(String s3keyPrefix);
 }
--- backend/tale/src/main/java/com/tosi/tale/s3/S3ServiceImpl.java
+++ backend/tale/src/main/java/com/tosi/tale/s3/S3ServiceImpl.java
@@ -49,8 +49,76 @@ public class S3ServiceImpl implements S3Service {
      */
     @Override
     public String findS3URL(String s3key) {
-        findS3Object(s3key);
         return amazonS3.getUrl(bucketName, s3key).toString();
+    }
+
+    /**
+     * AWS S3에서 해당 S3 Key의 파일(동화 본문)을 읽고 구간별로 반환합니다.
+     *
+     * @param s3Key S3 객체의 Key
+     * @return 구간별로 나뉜 동화 본문 배열(구분자: "-----")
+     * @throws CustomException 파일을 읽을 수 없는 경우 예외 처리
+     */
+    @Override
+    public String[] findContents(String s3Key) {
+        // try-with-resoures; 텍스트 파일을 버퍼로 읽고 자동으로 스트림을 닫음
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                findS3Object(s3Key).getObjectContent()))) {
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            // 파일 끝까지 한 줄씩 읽고 StringBuilder에 추가
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            return sb.toString().split("-----");
+        } catch (IOException e) {
+            throw new CustomException(ExceptionCode.TALE_CONTENT_FETCH_FAIL);
+        }
+    }
+
+    /**
+     * AWS S3에서 해당 S3 Key의 메타데이터(등장인물)를 반환합니다.
+     *
+     * @param s3Key S3 객체의 Key
+     * @return 디코딩된 등장인물 배열(구분자: 콤마)
+     * @throws CustomException 메타데이터를 가져올 수 없는 경우 예외 처리
+     */
+    @Override
+    public String[] findCharacters(String s3Key) {
+        String encodedCharacters = findS3Object(s3Key).getObjectMetadata().getUserMetadata().get("characters");
+        if (encodedCharacters == null) {
+            throw new CustomException(ExceptionCode.TALE_CHARACTERS_FETCH_FAIL);
+        }
+        return URLDecoder.decode(encodedCharacters, StandardCharsets.UTF_8).split(",");
+
+    }
+
+    /**
+     * AWS S3에서 해당 S3 Key의 Prefix로 동화별 폴더 내 삽화 파일의 주소 목록을 반환합니다.
+     *
+     * @param s3keyPrefix S3 객체의 Key Prefix
+     * @return 삽화 파일 주소(S3 객체 URL) 리스트
+     * @throws CustomException 폴더를 가져올 수 없는 경우 예외 처리
+     */
+    @Override
+    public List<String> findImages(String s3keyPrefix) {
+        // S3 버킷에서 주어진 prefix로 객체 목록을 조회하는 요청을 생성
+        ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .prefix(s3keyPrefix)
+                .build();
+
+        ListObjectsV2Response listObjectsV2Response = s3Client.listObjectsV2(listObjectsV2Request);
+        if(listObjectsV2Response == null){
+            throw new CustomException(ExceptionCode.TALE_IMAGES_FETCH_FAIL);
+        }
+
+        // S3 객체 리스트에서 각 객체의 Key로 S3 객체 URL를 생성한 후 추가
+        return listObjectsV2Response.contents().stream()
+                .map(s3Object -> findS3URL(s3Object.key()))
+                .toList();
     }
 
 
