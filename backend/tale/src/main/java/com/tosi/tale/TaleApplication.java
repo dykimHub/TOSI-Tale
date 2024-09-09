c447dac38f06baf969cb9c09f51e33c8573e0d8a
김다윤 <dy.jo.kim@gmail.com>
2024-09-09 14:39:35 +0900

    Chore: 동화 프로젝트 파일 정리

---
+++ backend/tale/src/main/java/com/tosi/tale/TaleApplication.java
@@ -0,0 +1,13 @@
+package com.tosi.tale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaleApplication.class, args);
+	}
+
+}
--- backend/tale/src/main/java/com/tosi/tale/tale/NoTalesFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tosi.tale.tale;
-
-public class NoTalesFoundException extends Exception {
-    public NoTalesFoundException(String message) {
-        super(message);
-    }
-}
