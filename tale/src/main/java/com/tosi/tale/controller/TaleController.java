a2f392bdb921ba951d0c1a512329c2a7f6d83d18
김다윤 <dy.jo.kim@gmail.com>
2024-10-16 20:39:18 +0900

    Chore: Swagger 요청 예시 추가

--- tale/src/main/java/com/tosi/tale/controller/TaleController.java
+++ tale/src/main/java/com/tosi/tale/controller/TaleController.java
@@ -6,11 +6,13 @@ import com.tosi.tale.dto.TalePageRequestDto;
 import com.tosi.tale.dto.TalePageResponseDto;
 import com.tosi.tale.service.TaleService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,8 +28,13 @@ public class TaleController {
 
     @Operation(summary = "동화 목록 조회")
     @GetMapping
-    public ResponseEntity<TaleDto.TaleDtos> findTaleList(@PageableDefault(size = 9, sort = "regDate", direction = Sort.Direction.DESC) Pageable pageable) {
-        // JPA에서 쿼리 파라미터를 읽어 Pageable 객체로 반환
+    public ResponseEntity<TaleDto.TaleDtos> findTaleList(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "9") int size,
+            @RequestParam(defaultValue = "regDate") String sort,
+            @RequestParam(defaultValue = "desc") String dir
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(dir), sort));
         TaleDto.TaleDtos taleDtoList = taleService.findTaleList(pageable);
         return ResponseEntity.ok()
                 .body(taleDtoList);
@@ -51,14 +58,27 @@ public class TaleController {
 
     @Operation(summary = "동화 제목으로 검색")
     @GetMapping("/search")
-    public ResponseEntity<List<TaleDto>> findTaleByTitle(@RequestParam String titlePart, @PageableDefault(size = 9, sort = "title") Pageable pageable) {
-        // JPA에서 쿼리 파라미터를 읽어 Pageable 객체로 반환
+    public ResponseEntity<List<TaleDto>> findTaleByTitle(
+            @Parameter(example = "여우")
+            @RequestParam String titlePart,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "9") int size,
+            @RequestParam(defaultValue = "title") String sort
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
         List<TaleDto> taleDtoList = taleService.findTaleByTitle(titlePart, pageable);
         return ResponseEntity.ok()
                 .body(taleDtoList);
     }
 
     @Operation(summary = "등장인물 이름을 매핑하고 각 페이지 생성")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                            value = ExampleObject.taleDetailExample
+                    )
+            )
+    )
     @PostMapping("/read")
     public ResponseEntity<List<TalePageResponseDto>> createTalePages(@RequestHeader("Authorization") String accessToken, @RequestBody TalePageRequestDto talePageRequestDto) {
         Long userId = taleService.findUserAuthorization(accessToken);
